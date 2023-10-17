838f9840af292378b417e3b91ec0a212e1307b62
milangupta95 <milangupta95@gmail.com>
2023-10-17 23:06:03 +0530

    Changed Code

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { MessagingResponse } = require('twilio').twiml;
-var request = require('request')
-var fs = require('fs')
-var filename = 'pic.png'
-var Tesseract = require('tesseract.js')
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const { createWorker } = require('tesseract.js');
 
 const app = express();
-const port = process.env.PORT || 3000;
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Initialize the Twilio client
+const accountSid = 'ACbc78c90f0241b895f9a24da9c662db20';
+const authToken = '3346c54df3d3b84b3e1e5a5e1e010e81';
+const client = require('twilio')(accountSid, authToken);
+
 app.post('/webhook', async (req, res) => {
-    const twiml = new MessagingResponse();
-    const message = twiml.message();
+  const twiml = new MessagingResponse();
 
-    if (req.body.NumMedia > 0) {
-        const url = req.body.MediaUrl0;
-        Tesseract.recognize(url)
-            .then(function (result) {
-                message.body(result.text);
-                console.log(result.text);
-            });
-    } else {
-        message.body('No image found.');
-    }
+  if (req.body.NumMedia > 0 && req.body.MediaContentType0.startsWith('image/')) {
+    const imageUrl = req.body.MediaUrl0;
 
-    res.writeHead(200, { 'Content-Type': 'text/xml' });
-    res.end(twiml.toString());
+    // Use Tesseract.js to extract text from the image.
+    const worker = createWorker();
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(imageUrl);
+    await worker.terminate();
+
+    // Send the extracted text as a WhatsApp response.
+    const responseMessage = `OCR Result: ${text}`;
+    twiml.message(responseMessage);
+
+    // Send the response via WhatsApp
+    client.messages.create({
+      to: req.body.From,
+      from: req.body.To,
+      body: responseMessage,
+    });
+  } else {
+    twiml.message("Please send an image for OCR.");
+  }
+
+  res.send(twiml.toString());
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
