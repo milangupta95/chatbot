ffac3e2b03b3bbf17a657b7232d235554181ebe1
milangupta95 <milangupta95@gmail.com>
2023-10-17 22:33:22 +0530

    Changed Code

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { MessagingResponse } = require('twilio').twiml;
-const { createWorker } = require('tesseract.js');
+var request = require('request')
+var fs = require('fs')
+var filename = 'pic.png'
+var Tesseract = require('tesseract.js')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,28 +12,32 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/webhook', async (req, res) => {
-  const twiml = new MessagingResponse();
-  const message = twiml.message();
+    const twiml = new MessagingResponse();
+    const message = twiml.message();
 
-  if (req.body.NumMedia > 0) {
-    const mediaUrl = req.body.MediaUrl0;
-    const worker = createWorker();
+    if (req.body.NumMedia > 0) {
+        const mediaUrl = req.body.MediaUrl0;
+        var writeFileStream = fs.createWriteStream(filename)
 
-    await worker.load();
-    await worker.loadLanguage('eng');
-    await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize(mediaUrl);
-    await worker.terminate();
+        request(url).pipe(writeFileStream).on('close', function () {
+            console.log(url, 'saved to', filename)
+        })
+        Tesseract.recognize(filename)
+            .progress(function (p) { console.log('progress', p) })
+            .catch(err => console.error(err))
+            .then(function (result) {
+                console.log(result.text)
+                message.body(result.text);
+                process.exit(0)
+            })
+    } else {
+        message.body('No image found.');
+    }
 
-    message.body(`Extracted Text: ${text}`);
-  } else {
-    message.body('No image found.');
-  }
-
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.end(twiml.toString());
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
